abe4fe979f7ba88a03832a0935deb450438893ef
Pojemnik <andrzej.gauza@outlook.com>
2024-11-16 18:09:06 +0100

    Add not working events

--- Scripts/menu_controller.gd
+++ Scripts/menu_controller.gd
@@ -2,6 +2,12 @@ extends Control
 
 @export var game_scene = "res://Scenes/SpawnpointSelection.tscn"
 
+func _ready() -> void:
+	$VBoxContainer/StartGameButton.pressed.connect(_on_start_game_pressed)
+	$VBoxContainer/CreditsButton.pressed.connect(_on_credits_button_pressed)
+	$VBoxContainer/QuitGameButton.pressed.connect(_on_quit_game_pressed)
+
+
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file(game_scene)
 
