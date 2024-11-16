8b48b8dd6dd5ef5850e53fd7728fd0cc1b47626f
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2024-11-16 10:59:01 +0100

    Adjust settings

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -7,7 +7,7 @@ func _on_clock_scene_kill_time_end() -> void:
 	if(state == game_state.npc_killed):
 		state = game_state.free
 	else:
-		print("YOU LOST!")
+		game_over()
 
 
 func _on_clock_scene_kill_time_start() -> void:
@@ -15,6 +15,17 @@ func _on_clock_scene_kill_time_start() -> void:
 
 
 func npc_killed(npc: Node) -> void:
-	if(state == game_state.kill_time):
-		state = game_state.npc_killed
-		print(npc.name + 'killed')
+	match state:
+		game_state.free:
+			game_over()
+		game_state.npc_killed:
+			game_over()
+		game_state.kill_time:
+			state = game_state.npc_killed
+			print(npc.name + 'killed')
+
+func game_over():
+	print("GAME OVER!")
+	await get_tree().create_timer(0.1).timeout
+	get_tree().quit()
+	
