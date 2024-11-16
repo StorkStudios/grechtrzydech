bd86e072d36f79dbb424fdb7cec8637845dc9b6c
Pojemnik <andrzej.gauza@outlook.com>
2024-11-17 00:32:53 +0100

    Add enemy death animation

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,13 @@
 class_name Enemy
 extends Node3D
 
+@export var animation : EnemyAnimation;
+var can_be_killed = true;
+
+
 func get_killed() -> void:
+	if (!can_be_killed):
+		return;
+	can_be_killed = false;
 	get_tree().get_first_node_in_group("GameManager").npc_killed(self)
-	self.queue_free()
+	animation.play_death_animation();
--- Scripts/enemy_animation.gd
+++ Scripts/enemy_animation.gd
@@ -1,11 +1,18 @@
 extends AnimationPlayer
+class_name EnemyAnimation
 
+signal death_animation_finished;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play("ArmatureAction")
+	play("Idle")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func play_death_animation():
+	play("Death")
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Death":
+		pause();
+		death_animation_finished.emit();
