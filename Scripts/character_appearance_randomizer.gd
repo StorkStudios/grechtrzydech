7acd5ba47aa757d3094687873609b08922e4f6de
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2024-11-17 00:38:12 +0100

    add random materials

--- Scripts/character_appearance_randomizer.gd
+++ Scripts/character_appearance_randomizer.gd
@@ -1,9 +1,22 @@
 extends Node
 
-var  top_head = 2
-var  moustache = 2
-var  beard = 1
-var  glasses = 1
+var tres = ".tres"
+var base_res_path = "res://Graphic/Materials/"
+var top_head = 2
+var moustache = 2
+var beard = 1
+var glasses = 1
+var accesories = 2
+var accesories_string = "CharacterAccesories"
+var eyes = 2
+var eyes_string = "CharacterEyes"
+var face = 2
+var face_string = "CharacterFace"
+var hair = 2
+var hair_string = "CharacterHair"
+var suit = 2
+var suit_string = "CharacterSuit"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +24,14 @@ func _ready() -> void:
 	randomize_glasses()
 	randomize_moustache()
 	randomize_top_head()
+	randomize_accesories()
+	randomize_eyes()
+	randomize_face()
+	randomize_hair()
+	randomize_suit()
 
-func randomize_enum_value(max: int) -> int:
-	return RandomNumberGenerator.new().randi_range(0, max)
+func randomize_enum_value(max: int, min = 0) -> int:
+	return RandomNumberGenerator.new().randi_range(min, max)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func randomize_beard():
 	var val = randomize_enum_value(beard)
@@ -32,3 +50,30 @@ func randomize_moustache():
 	var val = randomize_enum_value(moustache)
 	$Moustache.visible = (val == 1)
 	$MoustacheBeard.visible = (val == 2)
+
+func randomize_accesories():
+	var val = randomize_enum_value(accesories, 1)
+	$Glasses.set_surface_override_material(0, load(base_res_path + accesories_string + str(val) + tres))
+	$Cylinder.set_surface_override_material(0, load(base_res_path + accesories_string + str(val) + tres))
+
+func randomize_eyes():
+	var val = randomize_enum_value(eyes, 1)
+	$EyeLeft.set_surface_override_material(0, load(base_res_path + eyes_string + str(val) + tres))
+	$EyeRight.set_surface_override_material(0, load(base_res_path + eyes_string + str(val) + tres))
+	
+func randomize_face():
+	var val = randomize_enum_value(face, 1)
+	$Head.set_surface_override_material(0, load(base_res_path + face_string + str(val) + tres))
+	$Hands.set_surface_override_material(0, load(base_res_path + face_string + str(val) + tres))
+
+func randomize_hair():
+	var val = randomize_enum_value(hair, 1)
+	$Beard.set_surface_override_material(0, load(base_res_path + hair_string + str(val) + tres))
+	$Eyebrows.set_surface_override_material(0, load(base_res_path + hair_string + str(val) + tres))
+	$Hair.set_surface_override_material(0, load(base_res_path + hair_string + str(val) + tres))
+	$Moustache.set_surface_override_material(0, load(base_res_path + hair_string + str(val) + tres))
+	$MoustacheBeard.set_surface_override_material(0, load(base_res_path + hair_string + str(val) + tres))
+
+func randomize_suit():
+	var val = randomize_enum_value(suit, 1)
+	$Suit.set_surface_override_material(0, load(base_res_path + suit_string + str(val) + tres))
---
+++ Scripts/enemy_spawnpoint.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+enum type {standing}
+
+@export var character_type: type
