436d96e0f54a51a0dab24ad34d0d82f8212f0c6e
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2024-11-17 15:52:43 +0100

    Fix nan error

--- Scripts/music.gd
+++ Scripts/music.gd
@@ -36,12 +36,16 @@ func change_music(type: MusicEnum, animation_duration: float):
 	var time: float = 0;
 	while (time < animation_duration):
 		time += get_process_delta_time();
+		var descending: float = linear_to_db(lerp(music_volume, 0.0, time / animation_duration));
+		var rising: float = linear_to_db(lerp(0.0, music_volume, time / animation_duration));
+		descending = 0 if is_nan(descending) else descending;
+		rising = 0 if is_nan(rising) else rising;
 		if (use_second_player):
-			volume_db = linear_to_db(lerp(music_volume, 0.0, time / animation_duration));
-			second_player.volume_db = linear_to_db(lerp(0.0, music_volume, time / animation_duration));
+			volume_db = descending;
+			second_player.volume_db = rising;
 		else:
-			second_player.volume_db = linear_to_db(lerp(music_volume, 0.0, time / animation_duration));
-			volume_db = linear_to_db(lerp(0.0, music_volume, time / animation_duration));
+			second_player.volume_db = descending;
+			volume_db = rising;
 		await get_tree().process_frame;
 	
 	if (use_second_player):
