b1f63b64d8ebdbb95be6db3b51f9034502461015
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2024-11-17 02:05:21 +0100

    Add read mail

--- Scripts/mail_entry.gd
+++ Scripts/mail_entry.gd
@@ -9,5 +9,9 @@ const text_template: String = "from: {0}\nto: {1}\nsubject: {2}\n";
 var mail: Resource;
 
 func set_mail(mail_resourse: Resource) -> void:
+	mail = mail_resourse;
 	text = text_template.format([mail_resourse.from, mail_resourse.to, mail_resourse.subject]);
-	icon = mail_read_icon if mail_resourse.was_read else mail_unread_icon;
+
+func _process(delta: float) -> void:
+	if (mail):
+		icon = mail_read_icon if mail.was_read else mail_unread_icon;
