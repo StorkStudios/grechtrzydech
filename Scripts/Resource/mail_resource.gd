e039a4e2f0f92258732277c00f44e0fd835b8d0e
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2024-11-17 15:31:03 +0100

    Add different icons to mails

--- Scripts/Resource/mail_resource.gd
+++ Scripts/Resource/mail_resource.gd
@@ -5,5 +5,10 @@ class_name MailResource;
 @export var to: String;
 @export var subject: String;
 @export_multiline var body: String;
+@export var unread_icon: Texture2D;
+@export var read_icon: Texture2D;
 
 var was_read: bool = false;
+
+func get_mail_icon() -> Texture2D:
+	return read_icon if was_read else unread_icon;
--- Scripts/mail_entry.gd
+++ Scripts/mail_entry.gd
@@ -1,9 +1,6 @@
 class_name MailEntry;
 extends Button;
 
-@export var mail_unread_icon: Texture2D;
-@export var mail_read_icon: Texture2D;
-
 const text_template: String = "from: {0}\nto: {1}\nsubject: {2}\n";
 
 var mail: Resource;
@@ -14,4 +11,4 @@ func set_mail(mail_resourse: Resource) -> void:
 
 func _process(delta: float) -> void:
 	if (mail):
-		icon = mail_read_icon if mail.was_read else mail_unread_icon;
+		icon = mail.get_mail_icon();
