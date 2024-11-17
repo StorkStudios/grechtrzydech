48365707fc3b23d87c3c7457c3709a688c33cb5e
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2024-11-17 05:10:28 +0100

    Make mails work

--- Scripts/global_variables.gd
+++ Scripts/global_variables.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var player_start_pos: Vector3
+
+var mail_animation_state: int = 0;
+var mails: Array[Resource] = [];
--- Scripts/pc_display.gd
+++ Scripts/pc_display.gd
@@ -7,8 +7,23 @@ extends Control;
 @export var full_email: FullEmail;
 
 func _ready() -> void:
-	for mail_resource in mail_resources:
+	update_mails();
+	for mail_resource in GlobalVariables.mails:
 		var mail_entry := mail_instance.instantiate() as MailEntry;
 		mail_entry.set_mail(mail_resource);
 		mail_parent.add_child(mail_entry);
 		mail_entry.pressed.connect(func(): full_email.open_mail(mail_resource));
+
+func update_mails() -> void:
+	if (GlobalVariables.mail_animation_state == 1):
+		GlobalVariables.mails.append(mail_resources[0]);
+		GlobalVariables.mails.append(mail_resources.slice(2).pick_random());
+	elif (GlobalVariables.mail_animation_state == 2):
+		GlobalVariables.mails.append(mail_resources[1]);
+		GlobalVariables.mails.append(mail_resources.filter(func(x): return GlobalVariables.mails.find(x) < 0).pick_random());
+	elif (GlobalVariables.mail_animation_state > 2):
+		var mails_left := mail_resources.filter(func(x): return GlobalVariables.mails.find(x) < 0);
+		if (mails_left.size() > 0):
+			GlobalVariables.mails.append(mails_left.pick_random());
+	
+	GlobalVariables.mail_animation_state += 1;
