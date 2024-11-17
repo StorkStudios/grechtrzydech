a273f9aecc1b8fa3c0013bdfd7a0236fc9041838
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2024-11-17 03:01:12 +0100

    Key adjustments

--- Scripts/key.gd
+++ Scripts/key.gd
@@ -1,9 +1,8 @@
 extends Item
 
-@export var key_num: String = "NrKey01"
 @export var base_res_path = "res://Graphic/Materials/NrKey/"
 var tres = ".tres"
 
 func _ready() -> void:
 	for child: MeshInstance3D in $Key.get_children():
-		child.set_surface_override_material(0, load(base_res_path + key_num + tres))
+		child.set_surface_override_material(0, load(base_res_path + tag + tres))
